4f9fafcd4e0410f8e939fe1151112ff8d412de5c
Alim Naizabek <alim.naizabek@gmail.com>
2020-03-29 23:25:05 +0600

    cleanup

--- bin/main.dart
+++ bin/main.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:DartTarStream/DartTarStream.dart' as DartTarStream;
 
 void main(List<String> arguments) async {
-  final file = File("C:\\Projects\\DartTarStream\\ustar.tar");
+  final file = File('ustar.tar');
   final fileStream = file.openRead();
   final tarStream = DartTarStream.createStream(fileStream);
   final tarFile = await tarStream.single;
